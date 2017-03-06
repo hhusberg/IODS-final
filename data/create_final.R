5b1ba51beddf73a9a4446b81be466224a80b722a
Henrik Husberg <henrik.husberg@helsinki.fi>
2017-03-06 15:51:29 +0200

    New dataframe and new CFA

--- data/create_final.R
+++ data/create_final.R
@@ -101,8 +101,10 @@ complete.cases(kesky_subset)
 # print out the data along with a completeness indicator as the last column
 data.frame(kesky_subset[-1], comp = complete.cases(kesky_subset))
 
-# Ok, that didn't look quite as catastrophic as the first glance. I'll just go with a truncated dataset, 
-# using the cases that have complete data on the variables I'm actually going to analyze:
+# Ok, that didn't look quite as catastrophic as the first glance. I'll create two datasets, one with no missing values to use in the
+# analysis looking at the relationships beween the background information (i.e. media time and reading habits) and attention (as measured by
+# the ATTEX-questionnaire). The other dataset will contain all my cases and will be used to look at the factor structure of the ATTEX-
+# questionnaire using confirmatory factor analysis (CFA), because CFA is quite dependent on large sample sizes.
 
 # filter out all rows with NA values
 kesky_listwise <- filter(kesky_subset, complete.cases(kesky_subset) == TRUE)
@@ -170,6 +172,75 @@ summary(kesky_listwise)
 str(kesky_listwise)
 dim(kesky_listwise)
 
+# Now I'll do the same for the dataframe with the missing values:
+
+kesky_nona <- kesky_subset
+head(kesky_nona)
+dist_columns <- kesky_subset[2:5]
+head(dist_columns)
+kesky_nona$dist <- rowSums(dist_columns)
+kesky_nona$dist
+
+imp_columns <- kesky_subset[6:14]
+head(imp_columns)
+kesky_nona$imp <- rowSums(imp_columns)
+kesky_nona$imp
+
+moto_columns <- kesky_subset[15:21]
+head(moto_columns)
+kesky_nona$moto <- rowSums(moto_columns)
+kesky_nona$moto
+
+rikt_columns <- kesky_subset[22:26]
+head(rikt_columns)
+kesky_nona$rikt <- rowSums(rikt_columns)
+kesky_nona$rikt
+
+uppr_columns <- kesky_subset[27:32]
+head(uppr_columns)
+kesky_nona$uppr <- rowSums(uppr_columns)
+kesky_nona$uppr
+
+skif_columns <- kesky_subset[33:36]
+head(skif_columns)
+kesky_nona$skif <- rowSums(skif_columns)
+kesky_nona$skif
+
+efig_columns <- kesky_subset[37:41]
+head(efig_columns)
+kesky_nona$efig <- rowSums(efig_columns)
+kesky_nona$efig
+
+efpl_columns <- kesky_subset[42:45]
+head(efpl_columns)
+kesky_nona$efpl <- rowSums(efpl_columns)
+kesky_nona$efpl
+
+efutf_columns <- kesky_subset[46:53]
+head(efutf_columns)
+kesky_nona$efutf <- rowSums(efutf_columns)
+kesky_nona$efutf
+
+efutv_columns <- kesky_subset[54:56]
+head(efutv_columns)
+kesky_nona$efutv <- rowSums(efutv_columns)
+kesky_nona$efutv
+
+summary(kesky_nona)
+str(kesky_nona)
+dim(kesky_nona)
+names(kesky_nona)
+
+# In this dataset, I only want to keep the ATTEX sum variables (and the gender variable):
+
+remove5 <- names(kesky_nona[2:77])
+remove5
+library(dplyr)
+
+kesky_nona <- dplyr::select(kesky_nona, -one_of(remove5))
+names(kesky_nona)
+
+
 # Because one viable way of using the questionnaire data is a single sum variable, I'll go ahead and create a dataframe for that as well:
 
 attention_columns <- kesky_listwise[2:56]
@@ -177,18 +248,26 @@ head(attention_columns)
 kesky_listwise$attention <- rowSums(attention_columns) 
 summary(kesky_listwise$attention)
 
+# I'll be needing a dataset that contains the sums variables but not the original items, because it will make it much easier to
+# for example plot the whole dataframe. I'll create the dataframe kesky_sums for that:
+
 remove <- names(kesky_listwise[2:56])
 remove
-library(dplyr)
+
 kesky_sums <- dplyr::select(kesky_listwise, -one_of(remove))
 str(kesky_sums)
 names(kesky_sums)
+
+
+
 #And I'll remove the reading variables that I'm not interested in and only leave those related to homework (1 and 2) and literature (3 and 5)
 
 remove2 <- c("hur_ofta_lasbarn4", "hur_ofta_lasbarn6", "hur_ofta_lasbarn7", "hur_garna_lasbarn4", "hur_garna_lasbarn6", "hur_garna_lasbarn7")
 remove2
 kesky_sums <- dplyr::select(kesky_sums, -one_of(remove2))
 names(kesky_sums)
+
+
 
 # Last, I'll take the means  for homework related reading and leisure related reading, then remove the original variables:
 
@@ -206,9 +285,9 @@ kesky_sums$rd_pre_ls
 
 names(kesky_sums)
 
-remove2 <- names(kesky_sums[9:16])
-remove2
-kesky_sums <- dplyr::select(kesky_sums, -one_of(remove2))
+remove3 <- names(kesky_sums[9:16])
+remove3
+kesky_sums <- dplyr::select(kesky_sums, -one_of(remove3))
 names(kesky_sums)
 
 # And the means for the mothers and fathers education level to form my SES variable, then remove the original variables:
@@ -216,24 +295,28 @@ names(kesky_sums)
 kesky_sums$ses <- rowMeans(kesky_sums[4:5])
 kesky_sums$ses
 
-remove3 <- names(kesky_sums[4:5])
-remove3
-kesky_sums <- dplyr::select(kesky_sums, -one_of(remove3))
+remove4 <- names(kesky_sums[4:5])
+remove4
+kesky_sums <- dplyr::select(kesky_sums, -one_of(remove4))
 
 names(kesky_sums)
+
 
 # I'll save this as my new datafiles and use them for my final assignment. All other files will be removed.
 dim(kesky_listwise)
 head(kesky_listwise)
 write.csv2(kesky_listwise, file = "./data/final_cont.csv", row.names = FALSE)
 write.csv2(kesky_sums, file = "./data/final_sums_2.csv", row.names=FALSE)
+write.csv2(kesky_nona, file = "./data/final_nona.csv", row.names=FALSE)
 kesky_read <- read.csv2(file = "./data/final_cont.csv")
 sums_read <- read.csv2(file="./data/final_sums_2.csv")
+nona_read <- read.csv2(file="./data/final_nona.csv")
 dim(kesky_read)
 head(kesky_read)
 dim(sums_read)
 head(sums_read)
+dim(nona_read)
 library(compare)
 compare(kesky_sums, sums_read)
 compare(kesky_listwise, kesky_read)
-
+compare(kesky_nona, nona_read)
